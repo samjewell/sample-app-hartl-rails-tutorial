7812be1b64da0646363402cabd0c162a6a39d5a2
Sam Jewell <sam@gocardless.com>
2015-08-29 13:45:19 +0000

    less repetition (ex 3.1) & Contact page (ex 3.2)

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'static_pages#home' # controller - action pair
   get  'static_pages/help' # URL
   get  'static_pages/about' => 'static_pages#about' # URL pointing at controller-action pair
+  get  'static_pages/contact'
 end
