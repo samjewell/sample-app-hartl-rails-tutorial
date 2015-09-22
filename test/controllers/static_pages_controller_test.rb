be0ddf1975fe811fb1edab128cb80366d8268734
Sam Jewell <sam@gocardless.com>
2015-09-22 21:39:24 +0000

    Add my first Ruby module helper function, also add parens and curlies to stylesheet method call, and comment out failing test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
@@ -30,7 +30,7 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "Contact | #{@base_title}"
   end
   
-  test "foo" do
-    assert_equal 1, 2
-  end
+  # test "foo" do
+  #   assert_equal 1, 2
+  # end
 end
