5f7e2e54693211a88debe06b5c64756036e4aa34
Sam Jewell <sam.jewell@cantab.net>
2015-09-24 13:07:57 +0100

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home' # controller - action pair
-  get  'static_pages/help' # URL
-  get  'static_pages/about' => 'static_pages#about' # URL pointing at controller-action pair
-  get  'static_pages/contact'
+  get  'help' => 'static_pages#help' # URL
+  get  'about' => 'static_pages#about' # URL pointing at controller-action pair
+  get  'contact' => 'static_pages#contact'
+  get  'signup' => 'users#new'
 end
