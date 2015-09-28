c1e173cc9d6cf7c86659a5f08687fb7bb20cadf0
Sam Jewell <sam.jewell@cantab.net>
2015-09-28 15:19:43 +0200

    chapter 5 exercises

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -12,5 +8,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+  end
+
+  test "signup title" do
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
