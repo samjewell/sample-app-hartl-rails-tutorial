62aece6a3b31cee66abfb37ff8f91c3c13b9e999
Sam Jewell <sam.jewell@cantab.net>
2015-10-15 23:22:15 +0100

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  # Returns the full title on a per-page basis
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag gravatar_url, alt: user.name, class: "gravatar"
+  end
 end
